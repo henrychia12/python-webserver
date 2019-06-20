a62e963c6bf73ae602b1efd5ccb3b145df1310a6
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-20 13:04:00 +0100

    minor change to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,13 @@
-#!/bin/python3
+#!/bin/bash
 
 # create a user - pythonadm and create a home directory for it
 sudo useradd --create-home pythonadm
 
-# set jenkins default shell to python3
+# set pythonadm default shell to python3
 sudo usermod --shell /bin/python3 pythonadm
+
+# copy python and html file into user - pythonadm
+sudo cp ../homepage.html /home/pythonadm | sudo cp ../server.py /home/pythonadm
 
 # copy python.service file to systemd
 sudo cp ../python.service /etc/systemd/system
@@ -12,5 +15,5 @@ sudo cp ../python.service /etc/systemd/system
 # new service added, systemd needs to be reloaded
 sudo systemctl daemon-reload
 
-# start jenkins
+# start python
 sudo systemctl start python
